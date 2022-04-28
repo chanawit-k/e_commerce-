b6c0fcbb926cc9dcdfed46baa52390fcc3cc57f4
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-28 16:01:36 +0700

    add search

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Products (models.Model):
+
+    
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    images = models.CharField(max_length=300)
+
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Products
 # Create your views here.
+
+
+def index(request):
+    product_object = Products.objects.all()
+
+    search_product = request.GET.get('search_product')
+    if search_product != '' and search_product is not None:
+        product_object = product_object.filter(title__icontains = search_product )
+
+    return render(request, 'shop/index.html', {'product_object':product_object})
